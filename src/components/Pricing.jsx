4257c862a15264609c002a51e1c03e49441e113d
Shreyas <shreyassingh621@gmail.com>
2024-03-21 02:39:11 +0530

    BAsic layout prepared

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -28,8 +28,8 @@ const Pricing = () => {
         </div>
 
         <Heading
-          tag="Get started with Brainwave"
-          title="Pay once, use forever"
+          tag="Get started with AdimediaX"
+          title="Pay once, Repeat Again"
         />
 
         <div className="relative">
