4b0379ffbcf5a3f542bcf22b53174e1211c42f92
Shreyas <shreyassingh621@gmail.com>
2024-04-11 09:30:28 +0530

    Link Updated

--- src/components/Services2.jsx
+++ src/components/Services2.jsx
@@ -27,12 +27,13 @@ const Services2 = () => {
 
           <div className="relative z-1 grid gap-5 lg:grid-cols-2">
             <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
-              <iframe
-                title="Instagram Video"
-                src="https://www.instagram.com/reel/C5Q5DUKL-Kr/embed/"
-                allowFullScreen
-                className="w-full h-full"
-              ></iframe>
+            <iframe
+  title="Instagram Video"
+  src="https://www.instagram.com/reel/C4Lpkw6pQXY/embed/"
+  allowFullScreen
+  className="w-full h-full"
+></iframe>
+
             </div>
 
             <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
